2d9a1ef1f63bb381b894dcad03d10dcaf2f597bd
AbhijeetWaghmare <abhijeetwaghmare2000@gmail.com>
2025-10-06 14:19:01 +0000

    print page size

--- app/main.py
+++ app/main.py
@@ -9,11 +9,7 @@ command = sys.argv[2]
 
 if command == ".dbinfo":
     with open(database_file_path, "rb") as database_file:
-        # You can use print statements as follows for debugging, they'll be visible when running tests.
-        print("Logs from your program will appear here!")
-
-        # Uncomment this to pass the first stage
-        database_file.seek(16)  # Skip the first 16 bytes of the header
+        database_file.seek(16)  
         page_size = int.from_bytes(database_file.read(2), byteorder="big")
         print(f"database page size: {page_size}")
 else:
